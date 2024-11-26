c62c6766f2f259934ca16021e93e8489a6864541
Dmitrii Bulatov <bulati484@gmail.com>
2024-11-26 17:42:45 +0300

    add

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
 class Post(BaseModel):
     id: int
     title: str
     body: str
 
+
+users = [
+    {'id': 1, 'name': 'Дима', 'age': 40},
+    {'id': 2, 'name': 'Кристина', 'age': 35},
+    {'id': 3, 'name': 'Лёша', 'age': 42},
+]
+
 posts = [
-    {'id': 1, 'title': 'News 1', 'body': 'Text 1'},
-    {'id': 2, 'title': 'News 2', 'body': 'Text 2'},
-    {'id': 3, 'title': 'News 3', 'body': 'Text 3'},
+    {'id': 1, 'title': 'News 1', 'body': 'Text 1', 'author': users[3]},
+    {'id': 2, 'title': 'News 2', 'body': 'Text 2', 'author': users[1]},
+    {'id': 3, 'title': 'News 3', 'body': 'Text 3', 'author': users[2]},
 ]
 
 @app.get("/items")
